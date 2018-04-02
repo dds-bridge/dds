c624096496d5e52694cc40d375ba3bb0e286a343
Soren Hein <soren.hein@gmail.com>
2018-04-02 20:40:09 +0200

    Test program more or less done

--- test/testcommon.cpp
+++ test/testcommon.cpp
@@ -8,6 +8,10 @@
 */
 
 
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
 #include "../include/dll.h"
 #include "../include/portab.h"
 
@@ -21,6 +25,26 @@
 
 using namespace std;
 
+
+const vector<string> DDS_SYSTEM_PLATFORM =
+{
+  "",
+  "Windows",
+  "Cygwin",
+  "Linux",
+  "Apple"
+};
+
+const vector<string> DDS_SYSTEM_COMPILER =
+{
+  "",
+  "Microsoft Visual C++",
+  "MinGW",
+  "GNU g++",
+  "clang"
+};
+
+
 extern OptionsType options;
 TestTimer timer;
 
@@ -28,45 +52,28 @@ TestTimer timer;
 void main_identify();
 
 
-#define SOLVE_SIZE MAXNOOFBOARDS
-#define BOARD_SIZE MAXNOOFTABLES
-#define TRACE_SIZE MAXNOOFBOARDS
-#define PAR_REPEAT 1
-
-
 int realMain(int argc, char * argv[])
 {
   UNUSED(argc);
   UNUSED(argv);
-  timer.reset();
-  timer.setname("Hand stats");
-
   bool GIBmode = false;
 
-  // TODO: Make one.
   int stepsize = 0;
   if (options.solver == DTEST_SOLVER_SOLVE)
-    stepsize = SOLVE_SIZE;
+    stepsize = MAXNOOFBOARDS;
   else if (options.solver == DTEST_SOLVER_CALC)
-    stepsize = BOARD_SIZE;
+    stepsize = MAXNOOFTABLES;
   else if (options.solver == DTEST_SOLVER_PLAY)
-    stepsize = TRACE_SIZE;
+    stepsize = MAXNOOFBOARDS;
   else if (options.solver == DTEST_SOLVER_PAR)
-    stepsize = PAR_REPEAT;
+    stepsize = 1;
   else if (options.solver == DTEST_SOLVER_DEALERPAR)
-    stepsize = PAR_REPEAT;
+    stepsize = 1;
 
   set_constants();
   main_identify();
 
-  boardsPBN bop;
-  solvedBoards solvedbdp;
-  ddTableDealsPBN dealsp;
-  ddTablesRes resp;
-  allParResults parp;
-  playTracesPBN playsp;
-  solvedPlays solvedplp;
-
+  int number;
   int * dealer_list;
   int * vul_list;
   dealPBN * deal_list;
@@ -76,23 +83,33 @@ int realMain(int argc, char * argv[])
   parResultsDealer * dealerpar_list;
   playTracePBN * play_list;
   solvedPlay * trace_list;
-  int number;
-
   if (read_file(options.fname, number, GIBmode, &dealer_list, &vul_list,
         &deal_list, &fut_list, &table_list, &par_list, &dealerpar_list,
         &play_list, &trace_list) == false)
   {
-    printf("read_file failed.\n");
+    cout << "read_file failed\n";
     exit(0);
   }
 
+  if (GIBmode && options.solver != DTEST_SOLVER_CALC)
+  {
+    cout << "GIB file only works works with calc\n";
+    exit(0);
+  }
+
+  timer.reset();
+  timer.setname("Hand stats");
+
+  boardsPBN bop;
+  solvedBoards solvedbdp;
+  ddTableDealsPBN dealsp;
+  ddTablesRes resp;
+  allParResults parp;
+  playTracesPBN playsp;
+  solvedPlays solvedplp;
+
   if (options.solver == DTEST_SOLVER_SOLVE)
   {
-    if (GIBmode)
-    {
-      printf("GIB file only works works with calc\n");
-      exit(0);
-    }
     loop_solve(&bop, &solvedbdp, deal_list, fut_list, number, stepsize);
   }
   else if (options.solver == DTEST_SOLVER_CALC)
@@ -102,36 +119,21 @@ int realMain(int argc, char * argv[])
   }
   else if (options.solver == DTEST_SOLVER_PLAY)
   {
-    if (GIBmode)
-    {
-      printf("GIB file does not work with solve\n");
-      exit(0);
-    }
     loop_play(&bop, &playsp, &solvedplp, deal_list, play_list, trace_list, 
       number, stepsize);
   }
   else if (options.solver == DTEST_SOLVER_PAR)
   {
-    if (GIBmode)
-    {
-      printf("GIB file does not work with solve\n");
-      exit(0);
-    }
     loop_par(vul_list, table_list, par_list, number, stepsize);
   }
   else if (options.solver == DTEST_SOLVER_DEALERPAR)
   {
-    if (GIBmode)
-    {
-      printf("GIB file does not work with solve\n");
-      exit(0);
-    }
     loop_dealerpar(dealer_list, vul_list, table_list, dealerpar_list, 
       number, stepsize);
   }
   else
   {
-    printf("Unknown type %d\n", options.solver);
+    cout << "Unknown type " << options.solver << "\n";
     exit(0);
   }
 
@@ -151,37 +153,77 @@ int realMain(int argc, char * argv[])
 }
 
 
+//////////////////////////////////////////////////////////////////////
+//                     Self-identification                          //
+//////////////////////////////////////////////////////////////////////
+
+string GetSystem()
+{
+  unsigned sys;
+#if defined(_WIN32)
+  sys = 1;
+#elif defined(__CYGWIN__)
+  sys = 2;
+#elif defined(__linux)
+  sys = 3;
+#elif defined(__APPLE__)
+  sys = 4;
+#else
+  sys = 0;
+#endif
+  
+  return DDS_SYSTEM_PLATFORM[sys];
+}
+
+
+string GetBits()
+{
+  if (sizeof(void *) == 4)
+    return "32 bits";
+  else if (sizeof(void *) == 8)
+    return "64 bits";
+  else
+    return "unknown";
+}
+
+
+string GetCompiler()
+{
+  unsigned comp;
+#if defined(_MSC_VER)
+  comp = 1;
+#elif defined(__MINGW32__)
+  comp = 2;
+#elif defined(__clang__)
+  comp = 4; // Out-of-order on purpose
+#elif defined(__GNUC__)
+  comp = 3;
+#else
+  comp = 0;
+#endif
+
+  return DDS_SYSTEM_COMPILER[comp];
+}
+
+
 void main_identify()
 {
-  printf("dtest main\n----------\n");
+  cout << "test program\n";
+  cout << string(13, '-') << "\n";
 
-#if defined(_WIN32) || defined(__CYGWIN__)
-  printf("%-12s %20s\n", "System", "Windows");
-#if defined(_MSC_VER)
-  printf("%-12s %20s\n", "Compiler", "Microsoft Visual C++");
-#elif defined(__MINGW32__)
-  printf("%-12s %20s\n", "Compiler", "MinGW");
-#else
-  printf("%-12s %20s\n", "Compiler", "GNU g++");
-#endif
+  const string strSystem = GetSystem();
+  cout << left << setw(13) << "System" <<
+    setw(20) << right << strSystem << "\n";
 
-#elif defined(__linux)
-  printf("%-12s %20s\n", "System", "Linux");
-  printf("%-12s %20s\n", "Compiler", "GNU g++");
+  const string strBits = GetBits();
+  cout << left << setw(13) << "Word size" <<
+    setw(20) << right << strBits << "\n";
 
-#elif defined(__APPLE__)
-  printf("%-12s %20s\n", "System", "Apple");
-#if defined(__clang__)
-  printf("%-12s %20s\n", "Compiler", "clang");
-#else
-  printf("%-12s %20s\n", "Compiler", "GNU g++");
-#endif
-#endif
-
-#if defined(__cplusplus)
-  printf("%-12s %20ld\n", "Dialect", __cplusplus);
-#endif
-
-  printf("\n");
+  const string strCompiler = GetCompiler();
+  cout << left << setw(13) << "Compiler" <<
+    setw(20) << right << strCompiler << "\n\n";
 }
+
+
+
 
