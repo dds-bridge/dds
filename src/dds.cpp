e8b1cfe54926603fee86db8c75ca06d015c6ee4d
Larry Cai <larry.caiyu@gmail.com>
2019-11-09 20:11:51 +0100

    Add Patch from redeal
    * patch from https://github.com/anntzer/redeal/commit/4a1b95f7870cc7
    
    Signed-off-by: Larry Cai <larry.caiyu@gmail.com>

--- src/dds.cpp
+++ src/dds.cpp
@@ -34,7 +34,6 @@ extern "C" BOOL APIENTRY DllMain(
   else if (ul_reason_for_call == DLL_PROCESS_DETACH)
   {
     CloseDebugFiles();
-    FreeMemory();
 #ifdef DDS_MEMORY_LEAKS_WIN32
     _CrtDumpMemoryLeaks();
 #endif
@@ -59,7 +58,6 @@ void DDSInitialize(void)
 void DDSFinalize(void) 
 {
   CloseDebugFiles();
-  FreeMemory();
 }
 
 #elif defined(USES_CONSTRUCTOR)
@@ -73,7 +71,6 @@ static void __attribute__ ((constructor)) libInit(void)
 static void __attribute__ ((destructor)) libEnd(void)
 {
   CloseDebugFiles();
-  FreeMemory();
 }
 
 #endif
