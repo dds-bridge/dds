44a43b4952897616ed170d8ce822bf418413fb82
zzcgumn <n/a>
2025-09-21 13:04:48 +0200

    Removes tests that compared legacy and new heuristics.

--- library/tests/heuristic_sorting/test_utils.cpp
+++ library/tests/heuristic_sorting/test_utils.cpp
@@ -8,20 +8,6 @@
 #include "heuristic_sorting/heuristic_sorting.h"
 #include "heuristic_sorting/internal.h"
 
-// Minimal serializer: produce a simple JSON array of moves
-std::string serialize_move_list(const moveType* moves, int numMoves, bool include_scores) {
-  std::ostringstream out;
-  out << "[";
-  for (int i = 0; i < numMoves; ++i) {
-    if (i) out << ", ";
-    out << "{\"suit\":" << moves[i].suit << ",\"rank\":" << moves[i].rank;
-    if (include_scores) out << ",\"weight\":" << moves[i].weight;
-    out << "}";
-  }
-  out << "]";
-  return out.str();
-}
-
 // Normalization: stable textual representation (same as serialize for now)
 std::string normalize_ordering(const moveType* moves, int numMoves, bool include_scores) {
   // Create an index array and sort it deterministically by:
@@ -56,13 +42,6 @@ std::string normalize_ordering(const moveType* moves, int numMoves, bool include
   }
   out << "]";
   return out.str();
-}
-
-bool write_json_log(const std::string& path, const std::string& json_text) {
-  std::ofstream ofs(path);
-  if (!ofs) return false;
-  ofs << json_text;
-  return ofs.good();
 }
 
 // Initialize relRanks table and trackType based on a given pos (used by fuzz tests)
