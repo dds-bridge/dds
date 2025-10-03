1bb6fb74c72b340e941cbce27d55a50e747b8bfb
Martin Nygren <zzcgumn@mac.com>
2025-10-03 09:05:24 +0100

    test(system): add gtest for legacy vs context SolveBoard parity
    - New target: //library/tests/system:context_equivalence_test using GoogleTest
    - Verifies return code parity between legacy SolveBoard and SolveBoardWithContext
    - Remove standalone test wiring from root tests BUILD

---
+++ library/tests/system/context_equivalence_test.cpp
@@ -0,0 +1,35 @@
+#include <gtest/gtest.h>
+#include <cstring>
+
+#include "dds/dll.h"
+#include "dds/SolverContext.h"
+#include "system/Memory.h"
+
+extern Memory memory;
+
+static deal make_empty_deal()
+{
+  deal dl{};
+  dl.trump = 0;
+  dl.first = 0;
+  std::memset(dl.currentTrickSuit, 0, sizeof(dl.currentTrickSuit));
+  std::memset(dl.currentTrickRank, 0, sizeof(dl.currentTrickRank));
+  std::memset(dl.remainCards, 0, sizeof(dl.remainCards));
+  return dl;
+}
+
+TEST(SystemContextEquivalence, LegacyVsContextReturnCode)
+{
+  const int thr = 0;
+  futureTricks ft_legacy{};
+  futureTricks ft_ctx{};
+  deal dl = make_empty_deal();
+
+  const int r_legacy = SolveBoard(dl, /*target=*/0, /*solutions=*/1, /*mode=*/0, &ft_legacy, thr);
+
+  ThreadData* tdp = memory.GetPtr(static_cast<unsigned>(thr));
+  SolverContext ctx{tdp};
+  const int r_ctx = SolveBoardWithContext(ctx, dl, /*target=*/0, /*solutions=*/1, /*mode=*/0, &ft_ctx);
+
+  EXPECT_EQ(r_legacy, r_ctx) << "Legacy and context return codes should match";
+}
