c6f889816c3fb597cdc44efe62533f9b4472cbdc
Martin Nygren <zzcgumn@mac.com>
2026-01-27 09:43:35 +0000

    chore(Task 05): add api/solve_board.hpp forwarding header; retain PBN.h due to case-insensitive FS; update API BUILD

---
+++ library/src/api/solve_board.hpp
@@ -0,0 +1,7 @@
+#ifndef DDS_API_SOLVE_BOARD_HPP_LOWER
+#define DDS_API_SOLVE_BOARD_HPP_LOWER
+
+// Compatibility shim: prefer <api/solve_board.hpp>; forwards to legacy <api/SolveBoard.hpp>
+#include "SolveBoard.hpp"
+
+#endif // DDS_API_SOLVE_BOARD_HPP_LOWER
