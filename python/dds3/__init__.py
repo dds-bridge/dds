58ec683f70ef37868500f6ff449a564fd764d9d2
Martin Nygren <zzcgumn@mac.com>
2026-03-02 16:03:22 +0000

    creates a python wheel for easy install.

--- python/dds3/__init__.py
+++ python/dds3/__init__.py
@@ -1,10 +1,10 @@
-from _dds3 import api_root
-from _dds3 import calc_all_tables_pbn
-from _dds3 import calc_dd_table
-from _dds3 import module_name
-from _dds3 import par
-from _dds3 import solve_board
-from _dds3 import solve_board_pbn
+from ._dds3 import api_root
+from ._dds3 import calc_all_tables_pbn
+from ._dds3 import calc_dd_table
+from ._dds3 import module_name
+from ._dds3 import par
+from ._dds3 import solve_board
+from ._dds3 import solve_board_pbn
 
 __all__ = [
     "api_root",
