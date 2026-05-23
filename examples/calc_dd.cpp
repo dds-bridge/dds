96ff97ac94cbb01b2e2f1f5e84c166c6a5531f61
Adam Wildavsky <adam@tameware.com>
2026-05-23 22:08:32 +0200

    Read from stdin if available. Fix BUILD.bazel target name.

--- examples/calc_dd.cpp
+++ examples/calc_dd.cpp
@@ -16,11 +16,39 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
+#if defined(_WIN32)
+#include <io.h>
+#else
+#include <unistd.h>
+#endif
 #include <api/dll.h>
 #include "hands.hpp"
 
 
 #define PBN_FILE_MAX 8192
+
+
+static auto stdin_is_tty() -> bool
+{
+#if defined(_WIN32)
+  return _isatty(_fileno(stdin)) != 0;
+#else
+  return isatty(STDIN_FILENO) != 0;
+#endif
+}
+
+
+static auto read_pbn_stream(FILE * stream, char * buf, size_t buf_size) -> bool
+{
+  const size_t n = fread(buf, 1, buf_size - 1, stream);
+  if (n == 0)
+  {
+    return false;
+  }
+
+  buf[n] = '\0';
+  return true;
+}
 
 
 static auto read_pbn_file(const char * path, char * buf, size_t buf_size) -> bool
@@ -31,16 +59,9 @@ static auto read_pbn_file(const char * path, char * buf, size_t buf_size) -> boo
     return false;
   }
 
-  const size_t n = fread(buf, 1, buf_size - 1, f);
+  const bool ok = read_pbn_stream(f, buf, buf_size);
   fclose(f);
-
-  if (n == 0)
-  {
-    return false;
-  }
-
-  buf[n] = '\0';
-  return true;
+  return ok;
 }
 
 
@@ -93,12 +114,31 @@ static auto extract_deal_tag(const char * text, char * deal, size_t deal_size) -
 static auto load_deal(const char * arg, char * deal, size_t deal_size) -> int
 {
   char file_buf[PBN_FILE_MAX];
+  const char * source = arg;
 
-  if (read_pbn_file(arg, file_buf, sizeof(file_buf)))
+  if (strcmp(arg, "-") == 0)
+  {
+    source = "stdin";
+    if (!read_pbn_stream(stdin, file_buf, sizeof(file_buf)))
+    {
+      fprintf(stderr, "No PBN input on stdin\n");
+      return 1;
+    }
+  }
+  else if (read_pbn_file(arg, file_buf, sizeof(file_buf)))
+  {
+    source = arg;
+  }
+  else
+  {
+    source = nullptr;
+  }
+
+  if (source != nullptr)
   {
     if (!extract_deal_tag(file_buf, deal, deal_size))
     {
-      fprintf(stderr, "No [Deal \"...\"] tag found in %s\n", arg);
+      fprintf(stderr, "No [Deal \"...\"] tag found in %s\n", source);
       return 1;
     }
   }
@@ -118,18 +158,52 @@ static auto load_deal(const char * arg, char * deal, size_t deal_size) -> int
 }
 
 
+static auto print_usage(const char * prog) -> void
+{
+  fprintf(stderr,
+          "Usage: %s <pbn_deal_or_file>\n"
+          "       %s -h | --help\n"
+          "\n"
+          "Calculate double-dummy tricks for all strains and leads.\n"
+          "\n"
+          "Arguments:\n"
+          "  <pbn_deal_or_file>  DDS PBN deal string, or path to a .pbn file\n"
+          "\n"
+          "If stdin is not a terminal, PBN is read from stdin (uses [Deal \"...\"]).\n"
+          "\n"
+          "Examples:\n"
+          "  %s \"N:73.QJT.AQ54.T752 QT6.876.KJ9.AQ84 "
+          "5.A95432.7632.K6 AKJ9842.K.T8.J93\"\n"
+          "  %s hands/example.pbn\n"
+          "  %s < hands/example.pbn\n",
+          prog,
+          prog,
+          prog,
+          prog,
+          prog);
+}
+
+
 auto main(int argc, char * argv[]) -> int
 {
-  if (argc != 2)
+  const char * input;
+
+  if (argc == 2)
   {
-    fprintf(stderr,
-            "Usage: %s <pbn_deal_or_file>\n"
-            "Example: %s \"N:73.QJT.AQ54.T752 QT6.876.KJ9.AQ84 "
-            "5.A95432.7632.K6 AKJ9842.K.T8.J93\"\n"
-            "Example: %s hand.pbn\n",
-            argv[0],
-            argv[0],
-            argv[0]);
+    if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+    {
+      print_usage(argv[0]);
+      return 0;
+    }
+    input = argv[1];
+  }
+  else if (argc == 1 && !stdin_is_tty())
+  {
+    input = "-";
+  }
+  else
+  {
+    print_usage(argv[0]);
     return 1;
   }
 
@@ -137,7 +211,7 @@ auto main(int argc, char * argv[]) -> int
   DdTableResults table;
   char line[80];
 
-  if (load_deal(argv[1], tableDealPBN.cards, sizeof(tableDealPBN.cards)) != 0)
+  if (load_deal(input, tableDealPBN.cards, sizeof(tableDealPBN.cards)) != 0)
   {
     return 1;
   }
