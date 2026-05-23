12374b2a191ea8d99d9c65b363a1bc834bab540f
mortensp <2102113+mortensp@users.noreply.github.com>
2026-05-23 19:57:50 +0200

    Visual Studio og Rider support added in the form of .sln and .VCXprof files

--- library/src/dds.hpp
+++ library/src/dds.hpp
@@ -1,7 +1,12 @@
 #ifndef _DDS_EXTERNAL_CPP_API_H
 #define _DDS_EXTERNAL_CPP_API_H
 
-#include <dds/dds.h>
+#ifdef _VCXPROJ
+	#include <dds.h>
+#else
+	#include <dds/dds.h>
+#endif 
+
 #include <api/calc_dd_table.hpp>
 #include <api/calc_par.hpp>
 #include <api/solve_board.hpp>
