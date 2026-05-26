7837ef8f29d2792377c695fbda2360e599b0f430
mortensp <2102113+mortensp@users.noreply.github.com>
2026-05-26 07:55:27 +0200

    Typo fixed

--- library/src/dds.hpp
+++ library/src/dds.hpp
@@ -1,16 +1,16 @@
 #ifndef _DDS_EXTERNAL_CPP_API_H
 #define _DDS_EXTERNAL_CPP_API_H
 
- #if defined(__has_include)
- 	#if __has_include(<dds/dds.h>)
- 		#include <dds/dds.h>
- 	#elif __has_include(<dds.h>)
- 		#include <dds.h>
- 	#else
- 		#error "Unable to find DDS public header: expected <dds/dds.h> or <dds.h>"
- 	#endif
- #else
- 	#include <dds/dds.h>
+#if defined(__has_include)
+#if __has_include(<dds/dds.h>)
+#include <dds/dds.h>
+#elif __has_include(<dds.h>)
+#include <dds.h>
+#else
+#error "Unable to find DDS public header: expected <dds/dds.h> or <dds.h>"
+#endif
+#else
+#include <dds/dds.h>
 #endif
 
 #include <api/calc_dd_table.hpp>
