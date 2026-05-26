68397169bf9e930716f26269d8f49cfcb8dfaad9
mortensp <2102113+mortensp@users.noreply.github.com>
2026-05-26 09:52:40 +0200

    Initalt commit

---
+++ dotNet/AssemblyInfo.cpp
@@ -0,0 +1,20 @@
+#include "pch.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly:AssemblyTitleAttribute(L"DDSCS")];
+[assembly:AssemblyDescriptionAttribute(L"")];
+[assembly:AssemblyConfigurationAttribute(L"")];
+[assembly:AssemblyCompanyAttribute(L"")];
+[assembly:AssemblyProductAttribute(L"DDSCS")];
+[assembly:AssemblyCopyrightAttribute(L"Copyright (c)  2026")];
+[assembly:AssemblyTrademarkAttribute(L"")];
+[assembly:AssemblyCultureAttribute(L"")];
+
+[assembly:AssemblyVersionAttribute(L"1.0.*")];
+
+[assembly:ComVisible(false)];
---
+++ dotNet/DDS_CS.cpp
@@ -0,0 +1,4 @@
+#include "pch.h"
+
+#include "DDS_CS.h"
+
