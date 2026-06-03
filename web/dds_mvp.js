50f6863deb6d6f90f7b113ab4461b86652941102
Adam Wildavsky <adam@tameware.com>
2026-06-03 08:48:58 +0200

    Fix typo in comment.

--- web/dds_mvp.js
+++ web/dds_mvp.js
@@ -1,4 +1,4 @@
-// Copyright Copyright 2020-2026 Adam Wildavsky
+// Copyright 2020-2026 Adam Wildavsky
 //
 //   Use of this source code is governed by an MIT-style
 //   license that can be found in the LICENSE file or at
